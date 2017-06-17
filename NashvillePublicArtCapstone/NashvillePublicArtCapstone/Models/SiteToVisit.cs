b1297e35ed2664e2401ef2da0fa72176d4031589
abbie <abigail.c.holder@gmail.com>
2017-06-17 11:49:42 -0500

    database stuff

---
+++ NashvillePublicArtCapstone/NashvillePublicArtCapstone/Models/SiteToVisit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NashvillePublicArtCapstone.Models
+{
+    public class SiteToVisit
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Photo { get; set; }
+        public string ArtistFirstName { get; set; }
+        public string ArtistLastName { get; set; }
+        public string Location { get; set; }
+        public string Medium { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
